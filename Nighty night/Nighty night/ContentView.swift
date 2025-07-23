cefe5321a5961a23afa58f2a178451a1cc869572
Snoopy <oncwnuL2VoVxu4kfJqTp79aXZF5g@git.weixin.qq.com>
2025-07-24 00:20:37 +0800

    first page

---
+++ Nighty night/Nighty night/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nighty night
+//
+//  Created by Alex on 2025/7/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nighty night/Nighty night/Nighty_nightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nighty_nightApp.swift
+//  Nighty night
+//
+//  Created by Alex on 2025/7/19.
+//
+
+import SwiftUI
+
+@main
+struct Nighty_nightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
